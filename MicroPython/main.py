ec75ff6480203a34be2f60b3074606ce122b2c1a
sami-osman-2 <sami.osman@stu.ocsb.ca>
2025-12-15 14:00:37 -0500

    Update main.py

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,10 +1,17 @@
 """
-Created by: Mr. Coxall
-Created on: Sep 2020
-This module is a Micro:bit MicroPython program
+Created by: Sami Osman
+Created on: Dec 2025
+This module calcuales and outputs area and perimeter of a 5x3 rectangle.
 """
 
-from microbit import *
+length = 5
+width = 3
 
+area = length * width
+perimeter = 2 * (length + width)
 
-display.scroll("Hello, World!")
+print('\nLength:' + length + ' cm')
+print('\nWidth:' + width + ' cm')
+print('\n\nArea:' + area + ' 15 cm^2')
+print('\nPerimeter:' + perimeter + ' 16cm')
+print('\n\nDone.')
